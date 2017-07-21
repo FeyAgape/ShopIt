aa19797cc3125d791be3afc57da4b5ed0ee5f91c
Fey Ijaware <fshola4real@yahoo.co.uk>
2017-07-21 04:08:08 +0100

    First Commit

---
+++ app/src/main/java/com/example/android/shopit/InventoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.shopit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+}
